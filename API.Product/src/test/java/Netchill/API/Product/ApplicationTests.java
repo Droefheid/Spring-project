2e8928b91c0d818c6743796d230a24bc890073c6
Axl <axel.declerck@student.vinci.be>
2021-10-19 10:16:43 +0200

    add -all

---
+++ API.Product/src/test/java/Netchill/API/Product/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Netchill.API.Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
