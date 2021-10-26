3541140949d88689c0e5e04ab2b466f505789cff
corentin.decoster <corentin.decoster@student.vinci.be>
2021-10-26 10:19:18 +0200

    gitignore for .lib + Product model

--- API.Product/src/main/java/Netchill/API/Product/model/Product.java
+++ API.Product/src/main/java/Netchill/API/Product/model/Product.java
@@ -1,5 +1,24 @@
 package Netchill.API.Product.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity(name="products")
 public class Product {
-    int price;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String shortDescription;
+    private String longDescription;
+    private int price;
+    private String category;
+
+
 }
